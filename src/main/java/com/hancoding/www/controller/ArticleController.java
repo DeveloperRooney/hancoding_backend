42112520fc73e7cd6e7095277d54fd68c7b46cbf
DeveloperRooney <han_0427@naver.com>
2021-08-30 11:55:18 +0900

    페이징 처리

--- src/main/java/com/hancoding/www/controller/ArticleController.java
+++ src/main/java/com/hancoding/www/controller/ArticleController.java
@@ -3,6 +3,9 @@ package com.hancoding.www.controller;
 import com.hancoding.www.entity.Article;
 import com.hancoding.www.service.ArticleService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,8 +23,9 @@ public class ArticleController {
 
     // 게시글 리스트
     @GetMapping("/list")
-    public String articleList() {
+    public String articleList(Model model,@PageableDefault(size = 10, sort = "idx", direction = Sort.Direction.DESC) Pageable pageable) {
 
+        model.addAttribute("articleList", articleService.articleList(pageable));
         return "article/articlelist";
     }
 
--- src/main/java/com/hancoding/www/service/ArticleService.java
+++ src/main/java/com/hancoding/www/service/ArticleService.java
@@ -4,6 +4,8 @@ import com.hancoding.www.entity.Article;
 import com.hancoding.www.entity.Member;
 import com.hancoding.www.repository.ArticleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -14,9 +16,13 @@ public class ArticleService {
     @Autowired
     private ArticleRepository articleRepository;
 
+    public Page<Article> articleList(Pageable pageable) {
+
+        return articleRepository.findAll(pageable);
+    }
+
     public void insert(Article article) {
 
-//        article.setWriter(1);
         article.setProperty(1);
         article.setRegdate(LocalDateTime.now());
         article.setUpdate(LocalDateTime.now());
